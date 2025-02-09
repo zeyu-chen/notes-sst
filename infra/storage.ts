987fcd4df47535fed2ebb69851b3b82ab063b8d8
Zeyu Chen <iszeyu.chen@gmail.com>
2025-02-09 19:55:38 +1100

    feat: add dynamodb

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,11 @@
+// Create the S3 bucket
 export const bucket = new sst.aws.Bucket('Uploads');
+
+// Create the DynamoDB table
+export const table = new sst.aws.Dynamo('Notes', {
+  fields: {
+    userId: 'string',
+    noteId: 'string',
+  },
+  primaryIndex: { hashKey: 'userId', rangeKey: 'noteId' },
+});
--- sst-env.d.ts
+++ sst-env.d.ts
@@ -9,6 +9,10 @@ declare module "sst" {
       "type": "sst.aws.ApiGatewayV2"
       "url": string
     }
+    "Notes": {
+      "name": string
+      "type": "sst.aws.Dynamo"
+    }
     "Uploads": {
       "name": string
       "type": "sst.aws.Bucket"
