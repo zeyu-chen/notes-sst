b2c1dff0854e164523070c1adaa7a52acac44d81
Zeyu Chen <iszeyu.chen@gmail.com>
2025-02-09 19:50:12 +1100

    feat: rename template

--- sst.config.ts
+++ sst.config.ts
@@ -3,18 +3,14 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
+      name: 'notes',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      protect: ['production'].includes(input?.stage),
+      home: 'aws',
     };
   },
   async run() {
-    const storage = await import("./infra/storage");
-    await import("./infra/api");
-
-    return {
-      MyBucket: storage.bucket.name,
-    };
+    const storage = await import('./infra/storage');
+    await import('./infra/api');
   },
 });
